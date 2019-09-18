db1f8f18bef4756b0e8660461d687e2c661db822
ChongqingWangYu <1903372529@qq.com>
2019-09-18 08:27:10 +0800

    Lambda语法

--- src/Thread/LambdaThread/LambdaTest04.java
+++ src/Thread/LambdaThread/LambdaTest04.java
@@ -10,6 +10,7 @@ package Thread.LambdaThread;
 public class LambdaTest04 {
 
     public static void main(String[] args) {
+
         new Thread(() -> {
             System.out.println("一边学习Lambda");
         }).start();
