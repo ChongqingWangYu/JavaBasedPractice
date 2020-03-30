80cf6e8f8b786385c08093960a537fa75b160ce0
ChongqingWangYu <1903372529@qq.com>
2020-03-30 17:33:22 +0800

    序列化更新

--- src/myJava/io/SerializableTest.java
+++ src/myJava/io/SerializableTest.java
@@ -1,48 +1,45 @@
 package myJava.io;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 public class SerializableTest {
 
-	public static void main(String[] args) throws IOException,
-			ClassNotFoundException {
-		A a1 = new A(123, "abc");
-		String objectFile = "assets/io/Serializable/a1";
+    public static void main(String[] args) throws IOException,
+            ClassNotFoundException {
+        A a1 = new A(123, "abc");
+        String objectFile = "assets/io/Serializable/";
+        File file = new File(objectFile);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 对象序列化存为文件
+        FileOutputStream fos = new FileOutputStream(objectFile + "a1.obj");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(a1);
+        oos.close();
 
-		// 对象序列化存为文件
-		FileOutputStream fos = new FileOutputStream(objectFile);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(a1);
-		oos.close();
+        // 文件反序列化为对象
+        FileInputStream fis = new FileInputStream(objectFile + "a1.obj");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        A a2 = (A) ois.readObject();
+        ois.close();
+        System.out.println(a2);
 
-		// 文件反序列化为对象
-		FileInputStream fis = new FileInputStream(objectFile);
-		ObjectInputStream ois = new ObjectInputStream(fis);
-		A a2 = (A) ois.readObject();
-		ois.close();
-		System.out.println(a2);
+    }
 
-	}
+    private static class A implements Serializable {
 
-	private static class A implements Serializable {
+        private int x;
+        private String y;
 
-		private int x;
-		private String y;
+        A(int x, String y) {
+            this.x = x;
+            this.y = y;
+        }
 
-		A(int x, String y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public String toString() {
-			return "x = " + x + "  " + "y = " + y;
-		}
-	}
+        @Override
+        public String toString() {
+            return "x = " + x + "  " + "y = " + y;
+        }
+    }
 }
